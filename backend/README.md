4dd881a6e126d504812052a4c1f57d10aa268c82
AK Maurya <143257696+ankit-kr-maurya-82@users.noreply.github.com>
2025-12-23 22:30:44 +0530

    util code

--- backend/README.md
+++ backend/README.md
@@ -3,3 +3,4 @@ backend <br>
 └─ npm i -D prettier <br>
 db atlas
 db compass
+└─ npm i cookie-parser cors
--- backend/src/index.js
+++ backend/src/index.js
@@ -6,3 +6,13 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=> {
+    app.listen(process.env.PORT || 3000, () => {
+        console.log(`☸️ Server is running at port : ${process.env.PORT}`);
+        
+    })
+})
+.catch((err)=> {
+    console.log("MONGO DB connections error !!!", err);
+    
+})
