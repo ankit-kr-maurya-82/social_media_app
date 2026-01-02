5141b9381506b4a6027ff74a53bcec47fbd74829
AK Maurya <143257696+ankit-kr-maurya-82@users.noreply.github.com>
2026-01-02 14:04:52 +0530

    fix

--- backend/src/models/Channel.model.js
+++ backend/src/models/Channel.model.js
@@ -1,12 +1,33 @@
-import mongoose ,{Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
 const channelSchema = new Schema(
-    {
-        name: String
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    {
-        timestamps: true
-    }
-)
+
+    serverId: {
+      type: Schema.Types.ObjectId,
+      ref: "Server",
+      required: true,
+    },
+
+    type: {
+      type: String,
+      enum: ["text", "voice"],
+      default: "text",
+    },
+
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 export const Channel = mongoose.model("Channel", channelSchema);
