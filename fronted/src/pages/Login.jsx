aa8b046ae3ac8882634dae271d4aaff59a620aa0
AK Maurya <143257696+ankit-kr-maurya-82@users.noreply.github.com>
2026-01-04 21:59:27 +0530

    login design

--- fronted/src/pages/Login.jsx
+++ fronted/src/pages/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import UserContext from "../context/UserContext";
 import api from "../api/axios.js";
+import "./CSS/Login.css"
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -40,17 +41,17 @@ const Login = () => {
   };
 
   return (
-    <div className="h-screen flex justify-center items-center bg-gradient-to-br from-[#020617] to-[#0f172a]">
-      <form className="bg-[#020617] text-white p-6 rounded-2xl shadow-xl w-[340px] flex flex-col gap-4 border border-gray-800">
-        <h2 className="text-2xl font-bold text-center">Login</h2>
-        {errorMsg && <p className="text-red-500 text-sm text-center">{errorMsg}</p>}
+    <div className="login-container">
+      <form className="login-form">
+        <h2 className="">Login</h2>
+        {errorMsg && <p className="error-msg">{errorMsg}</p>}
 
         <input
           type="email"
           placeholder="Email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-          className="bg-[#020617] border border-gray-700 px-4 py-3 rounded-xl outline-none focus:border-blue-500"
+          className=""
           required
         />
         <input
@@ -58,19 +59,19 @@ const Login = () => {
           placeholder="Password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          className="bg-[#020617] border border-gray-700 px-4 py-3 rounded-xl outline-none focus:border-blue-500"
+          className=""
           required
         />
         <button
           type="submit"
           onClick={handleSubmit}
           disabled={loading}
-          className="mt-2 bg-gradient-to-r from-blue-600 to-indigo-600 hover:opacity-90 transition py-3 rounded-xl font-semibold"
+          className="login-btn"
         >
           {loading ? "Logging in..." : "Login"}
         </button>
 
-        <p className="text-sm text-center text-gray-400 mt-2">
+        <p className="login-footer">
           Don't have an account?{" "}
           <Link to="/register" className="text-blue-400 hover:underline">
             Register
