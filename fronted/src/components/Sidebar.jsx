d5c0e501c537424dd3e31f98e9098481aa15fc01
AK Maurya <143257696+ankit-kr-maurya-82@users.noreply.github.com>
2026-01-07 18:10:29 +0530

    sidebar ui change like header

--- fronted/src/components/Sidebar.jsx
+++ fronted/src/components/Sidebar.jsx
@@ -1,25 +1,63 @@
 import React, { useContext, useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
-import { FaBars, FaPlus, FaHome, FaUser, FaCompass } from "react-icons/fa";
+import { FaBars, FaPlus, FaHome, FaUser, FaCompass, FaSignOutAlt } from "react-icons/fa";
 import UserContext from "../context/UserContext";
 import "./CSS/Sidebar.css";
 
 const Sidebar = () => {
   const { user, setUser } = useContext(UserContext);
   const navigate = useNavigate();
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(true);
 
   const handleLogout = () => {
     localStorage.removeItem("user");
     setUser(null);
     navigate("/login");
-    setOpen(false);
   };
 
   return (
-  <div>
-    
-  </div>
+    <aside className={`sidebar ${open ? "open" : "collapsed"}`}>
+      
+      {/* Toggle */}
+      <div className="sidebarToggle" onClick={() => setOpen(!open)}>
+        <FaBars />
+      </div>
+
+      {/* Menu */}
+      <nav className="sidebarMenu">
+        <NavLink to="/" className="sidebarItem">
+          <FaHome />
+          <span>Home</span>
+        </NavLink>
+
+        <NavLink to="/explore" className="sidebarItem">
+          <FaCompass />
+          <span>Explore</span>
+        </NavLink>
+
+        {user && (
+          <NavLink to="/profile" className="sidebarItem">
+            <FaUser />
+            <span>Profile</span>
+          </NavLink>
+        )}
+
+        <NavLink to="/create" className="sidebarItem">
+          <FaPlus />
+          <span>Create</span>
+        </NavLink>
+      </nav>
+
+      {/* Footer */}
+      {user && (
+        <div className="sidebarFooter">
+          <button onClick={handleLogout} className="sidebarItem logout">
+            <FaSignOutAlt />
+            <span>Logout</span>
+          </button>
+        </div>
+      )}
+    </aside>
   );
 };
 
