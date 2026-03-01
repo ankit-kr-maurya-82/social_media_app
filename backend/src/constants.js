363bed897b682b082d5faa2c2668f70c59bbd48b
AK Maurya <ankitkrmaurya82@gmail.com>
2026-03-01 22:33:29 +0530

    add about page

--- backend/src/constants.js
+++ backend/src/constants.js
@@ -1,1 +1,1 @@
-export const DB_NAME = "social_tweets"
+export const DB_NAME = "caeme"
--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,17 +1,26 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "../constants.js";
 
-const connectDB = async () => {
-    try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-        console.log(`\n MongoDB connected !! DB Host: ${connectionInstance.connection.host}`);
-            console.log("MongoDB Connected:", mongoose.connection.name);
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://ankitkrmaurya82_db_user:anki@cluster0.visghtc.mongodb.net/?appName=Cluster0";
 
-    } catch (error) {
-        console.log("MONGODB connection FAILED", error);
-        process.exit(1)
-        
-    }
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
 }
-
-export default connectDB;
+run().catch(console.dir);
