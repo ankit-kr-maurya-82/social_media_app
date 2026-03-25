f635f2785facb8233b4c388a2ac8f91240151931
AK Maurya <ankitkrmaurya82@gmail.com>
2026-03-25 20:54:13 +0530

    features bg fx

--- fronted/src/components/FeatureArticle .jsx
+++ fronted/src/components/FeatureArticle .jsx
@@ -4,19 +4,21 @@ import "./CSS/FeatureArticle.css";
 const FeatureArticle = () => {
   return (
     <div className="main_article">
-      
-      {/* Date Badge */}
-      <div className="date-container">
-        <p className="month">Jun</p>
-        <p className="date">23</p>
-      </div>
 
-      {/* Image */}
-      <div className="left-article-section">
-        <img
-          src="https://plus.unsplash.com/premium_photo-1673697240073-04416dc18ab3?q=80&w=1170"
-          alt="article"
-        />
+      <div className="left_container">
+        {/* Date Badge */}
+        {/* <div className="date-container">
+          <p className="month">Jun</p>
+          <p className="date">23</p>
+        </div> */}
+
+        {/* Image */}
+        <div className="left-article-section">
+          <img
+            src="https://plus.unsplash.com/premium_photo-1673697240073-04416dc18ab3?q=80&w=1170"
+            alt="article"
+          />
+        </div>
       </div>
 
       {/* Content */}
@@ -26,8 +28,7 @@ const FeatureArticle = () => {
           1015 California Ave, Los Angeles CA · 7:00 pm - 8:00 pm
         </p>
         <p className="desc">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
-          blanditiis tenetur ullam suscipit!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
         </p>
 
         <hr />
